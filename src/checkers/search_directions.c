968f42061ffdf8d7893e72b201beef8d5a2f3b00
Paulo Victor <pauvicto@student.42sp.org.br>
2023-11-09 01:20:34 +0100

    header e ultimos fix

--- src/checkers/search_directions.c
+++ src/checkers/search_directions.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   search_directions.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pauvicto <pauvicto@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 01:01:10 by pauvicto          #+#    #+#             */
+/*   Updated: 2023/11/09 01:19:24 by pauvicto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
 int	is_valid_position(int i, int j, t_scale scale)
@@ -7,8 +19,11 @@ int	is_valid_position(int i, int j, t_scale scale)
 
 int	search_zero_right(t_list_map head, int i, int j, t_scale scale)
 {
-	if (j + 1 == scale.width || head.map[i][j + 1] == 32)
-		return (0);
+	if (head.map[i][j] != '1')
+	{
+		if (j + 1 == scale.width || head.map[i][j + 1] == 32)
+			return (0);
+	}
 	if (is_valid_position(i, j + 1, scale))
 	{
 		if (head.map[i][j + 1] == '0')
@@ -22,8 +37,11 @@ int	search_zero_right(t_list_map head, int i, int j, t_scale scale)
 
 int	search_zero_down(t_list_map head, int i, int j, t_scale scale)
 {
-	if (i + 1 == scale.height || head.map[i + 1][j] == 32)
-		return (0);
+	if (head.map[i][j] != '1')
+	{
+		if (i + 1 == scale.height || head.map[i + 1][j] == 32)
+			return (0);
+	}
 	if (is_valid_position(i + 1, j, scale))
 	{
 		if (head.map[i + 1][j] == '0')
@@ -37,8 +55,11 @@ int	search_zero_down(t_list_map head, int i, int j, t_scale scale)
 
 int	search_zero_left(t_list_map head, int i, int j, t_scale scale)
 {
-	if (j == 0 || head.map[i][j - 1] == 32)
-		return (0);
+	if (head.map[i][j] != '1')
+	{
+		if (j == 0 || head.map[i][j - 1] == 32)
+			return (0);
+	}
 	if (is_valid_position(i, j - 1, scale))
 	{
 		if (head.map[i][j - 1] == '0')
--- src/cub3d.c
+++ src/cub3d.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pauvicto <pauvicto@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 01:02:16 by pauvicto          #+#    #+#             */
+/*   Updated: 2023/11/09 01:11:16 by pauvicto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <cub3d.h>
 
 int	main(int argc, char **argv)
@@ -7,10 +19,8 @@ int	main(int argc, char **argv)
 	ft_bzero(&cub3d, sizeof(t_cub3D));
 	if (argc != 2)
 		error_message(ARG_ERR);
-	printf("argc: OK \n");
 	if (check_map(&cub3d, argv) == FALSE)
 		return (1);
-	printf("check_map: OK \n");
 	init_game(&cub3d);
 	return (0);
 }
