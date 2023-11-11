68f5dacdfc0aa650646bfd5a2ff24c7076e9b135
Henrique P. Bernardes <91508375+HenriqueBernardes42@users.noreply.github.com>
2023-11-10 21:06:50 -0300

    Update check_map.c
    colocado um sina de '='

--- src/checkers/check_map.c
+++ src/checkers/check_map.c
@@ -102,7 +102,7 @@ int	check_map(t_cub3D *cub3D, char **argv)
 	if (fd < 0)
 		return (FALSE);
 	n_lines = process_lines(list, fd);
-	if(n_lines < 7)
+	if(n_lines <= 7)
 	{
 		error_message(INCOMPLETE_FILE);
 		destroy_list(&list);
